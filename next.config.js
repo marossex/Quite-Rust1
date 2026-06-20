5ff9a9b60b94a594a04b6e57f6f0faf431488688
marossex <marosswere@gmail.com>
2026-06-21 02:04:32 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/quite-rust-site',
+  assetPrefix: '/quite-rust-site/',
+}
+
+module.exports = nextConfig
